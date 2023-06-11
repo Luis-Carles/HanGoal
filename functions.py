cc9f15fbae61473cfe47562e9fe4accf4807ef1e
Luis-Carles <lcarles4@hotmail.com>
2023-06-11 22:10:36 +0200

    basic changes

---
+++ functions.py
@@ -0,0 +1,18 @@
+def setResponse(Phrase r):
+    #verify for duplicates
+    #check for question
+      #create new entry if needed
+    #actualize answer
+
+
+def getResponse(Phrase r):
+    #local
+    #GTranslate
+    #Compare them
+    #Actualize the BD in local if neccesary
+    #return the answer
+
+def requestAPI(Phrase p):
+    #Making the request to API Rest
+
+
